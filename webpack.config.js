71e669eb62d55faba12c9c89ac72e23f8fc74cdc
janlin002 <123davidbill@gmail.com>
2022-05-05 20:54:44 +0800

    webpack eslint

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,22 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode:"development",
-  entry: path.join(__dirname, "src", "index.js"),
+  mode: 'development',
+  entry: path.join(__dirname, 'src', 'index.js'),
   output: {
-    path:path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    alias: {
+      Assets: path.resolve(__dirname, 'src/assets'),
+      Components: path.resolve(__dirname, 'src/components'),
+      Constants: path.resolve(__dirname, 'src/constants'),
+      Hooks: path.resolve(__dirname, 'src/hooks'),
+      Pages: path.resolve(__dirname, 'src/pages'),
+      Redux: path.resolve(__dirname, 'src/redux'),
+      Routes: path.resolve(__dirname, 'src/routes'),
+      Utils: path.resolve(__dirname, 'src/utils'),
+    },
   },
   module: {
     rules: [
@@ -13,25 +25,25 @@ module.exports = {
         test: /\.?js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jp(e*)g|svg|gif)$/,
         use: ['file-loader'],
       },
-    ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
+      template: path.join(__dirname, 'public', 'index.html'),
     }),
   ],
-}
+};
